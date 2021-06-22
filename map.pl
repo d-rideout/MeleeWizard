8ea11df308bf1ee1c04a32b163fa8c7679561bf5
David Rideout <ufahamu@pm.me>
2021-06-21 18:49:01 -0700

    Attempting to write coordinates in hexes -- not finished

--- map.pl
+++ map.pl
@@ -4,17 +4,29 @@ use strict;
 use warnings;
 
 my $n = 7;
-# for (0..6) {
+my $x;
+my $y = 'a';
 print ' --', '    --'x$n, "\n";
 
-for (0..0) {
+for (0..1) {
+  $x = 'a';
   print '/  \\', '  /  \\'x$n, "\n";
-  print ' xx', ' -- xx'x$n, "\n";
+  print " $x$y", (" -- ".++$x.$y)x$n, "\n";
   print '\\  /', '  \\  /'x$n, "\n";
-  print ' --', ' xx --'x$n, "\n";
+  ++$y;
+  print ' --', " $x$y --"x$n, "\n";
 }
-
+++$y;
 print '/  \\', '  /  \\'x$n, "\n";
-print ' xx', ' -- xx'x$n, "\n";
+print " $x$y", " -- $x$y"x$n, "\n";
 print '\\  /', '  \\  /'x$n, "\n";
 print ' --', '    --'x$n, "\n";
+
+
+# 't' for top hex; 'b' for bottom; or $x$y of SW hex for neither
+sub draw_hex {
+  my $x = shift;
+  my $y = shift;
+  my $border = shift;
+
+  ...
